2dafbd388fe841f226485d799034387fdccdb22f
MisBlock <95900291+misblocdev@users.noreply.github.com>
2021-12-11 12:44:50 +0900

    Misblock Application V1.0

---
+++ store/store.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import {createStore, applyMiddleware} from 'redux';
+import { createLogger } from 'redux-logger';
+import { persistStore, persistReducer} from 'redux-persist';
+
+import rootReducer from '../reducers/index';
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: [
+        'wallet', 
+        'accountData',
+        'unreadAlerts'
+    ],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(
+    persistedReducer,
+    // applyMiddleware(
+    //     createLogger(),
+    // ),
+);
+
+let persistor = persistStore(store);
+
+export {store, persistor};
