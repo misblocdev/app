fe0a45e82d72f68c3424fc4e93b2f03f7f3d9371
MisBlock <95900291+misblocdev@users.noreply.github.com>
2021-12-11 12:42:05 +0900

    Misblock Application V1.0

---
+++ constants/Colors.js
@@ -0,0 +1,6 @@
+export const BG_COLOR = "white";
+export const WHITE = "white";
+export const SKYBLUE = "#00bfff";
+export const GREY_COLOR = "#bdc3c7";
+export const INACTIVE_COLOR = "#7f8c8d";
+export const ACTIVE_COLOR = SKYBLUE;
